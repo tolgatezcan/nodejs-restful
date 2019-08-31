c73fc27acf04d8cd9c8219c82d6486321148d6e6
Tolga Tezcan <tolgaa.tezcan@gmail.com>
2019-08-31 20:35:07 +0300

    added hash log file

--- models/client.js
+++ models/client.js
@@ -1,5 +1,8 @@
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
+const fs = require('fs');
+const moment = require('moment');
+const dotenv = require("dotenv").config(); //.env setting
 const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017';
 const dbName = 'restful';
@@ -14,7 +17,7 @@ exports.login = function (req, res, next) {
                 var query = { email: req.body.email, password: req.body.password };
                 db.collection("users").findOne( query, function(err, result) {
                     if (!err && result != null) {
-                        var token = jwt.sign({ data: result }, 'secret', { expiresIn: '1m' });
+                        var token = jwt.sign({ data: result }, 'secret', { expiresIn: '30m' });
                         resolve({ token: token });
                     }
                     else 
@@ -36,7 +39,7 @@ exports.register = function (req, res, next) {
                 var query = { email: req.body.email, password: req.body.password };
                 db.collection("users").insertOne( query, function(err, result) {
                     if (!err) {
-                        var token = jwt.sign({ data: result.ops }, 'secret', { expiresIn: '1m' });
+                        var token = jwt.sign({ data: result.ops }, 'secret', { expiresIn: '30m' });
                         resolve({ token: token });
                     }
                     else 
@@ -50,9 +53,12 @@ exports.register = function (req, res, next) {
 
 exports.hash = function (req, res, next) {
     return new Promise(function (resolve) {
-        var current_date = (new Date()).valueOf().toString();
+        var current_date = moment().format("YYYY-MM-DD HH:mm:ss");
         var random = Math.random().toString();
         var hash = crypto.createHash('sha1').update(current_date + random).digest('hex');
+        var filename = __dirname + "/../" + process.env.LOGFILE;
+        var log_file  = fs.createWriteStream(filename, { flags: "a" });
+        log_file.write("["+current_date+"]" + " HASH.INFO: " + hash + '\n');
         resolve({ hash: hash });
     });
 };

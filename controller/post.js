f595bf76ad7932b027a3ca93df66edbd4b5747a6
tolgatezcan <tolgaa.tezcan@gmail.com>
2019-12-02 17:22:41 -0500

    @hapi/joi added

--- controller/post.js
+++ controller/post.js
@@ -1,43 +1,60 @@
 const jwt = require('jsonwebtoken');
 const moment = require('moment');
-const crypto = require('crypto');
+const bcrypt = require('bcryptjs');
 const fs = require('fs');
 const User = require('../models/User');
+const {loginValidation} = require('../models/Validation');
+const {registerValidation} = require('../models/Validation');
 
 exports.login = async function (req, res, next) {
-    const post = {
-        email: req.body.email,
-        password: req.body.password
-    };
-
-    try {
-        const loginUser = await User.findOne(post);
-        if (loginUser != null) {
-            const token = jwt.sign({ data: loginUser }, 'secret', { expiresIn: '30m' });
-            res.json({id_token: token});
+    const {error} = loginValidation(req.body);
+    if (error) res.status(400).json({message: error.details[0].message});
+    else {
+        const post = {
+            email: req.body.email,
+            password: req.body.password
+        };
+    
+        try {
+            const loginUser = await User.findOne(post);
+            if (loginUser != null) {
+                const token = jwt.sign({ data: loginUser }, 'secret', { expiresIn: '30m' });
+                res.status(200).json({id_token: token});
+            }
+            else {
+                res.status(400).json({message: 'User not found'});
+            }
         }
-        else {
-            res.json({message: 'User not found'});
+        catch (err) {
+            res.status(400).json(err);
         }
-    }
-    catch (err) {
-        res.json(err);
     }
 };
 
 exports.register = async function (req, res, next) {
-    const post = new User({
-        email: req.body.email,
-        password: req.body.password
-    });
-
-    try {
-        const registerUser = await post.save();
-        const token = jwt.sign({ data: registerUser }, 'secret', { expiresIn: '30m' });
-        res.json({id_token: token});
-    }
-    catch (err) {
-        res.json(err);
+    const {error} = registerValidation(req.body);
+    if (error) res.status(400).json({message: error.details[0].message});
+    else {
+        const emailCheck = await User.findOne({email: req.body.email});
+        if (emailCheck) res.status(400).json({message: 'Email already exists'});
+        else {
+            const salt = await bcrypt.genSalt(10);
+            const hashPassword = await bcrypt.hash(req.body.password, salt);
+            
+            const post = new User({
+                email: req.body.email,
+                password: hashPassword
+            });
+        
+            try {
+                const registerUser = await post.save();
+                const token = jwt.sign({ data: registerUser }, 'secret', { expiresIn: '30m' });
+                res.status(200).json({id_token: token});
+            }
+            catch (err) {
+                res.status(400).json(err);
+            }
+        }
     }
 };
 

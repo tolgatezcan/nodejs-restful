989b4b6d013862b08c0a861028c862c1d68dd910
tolgatezcan <tolgaa.tezcan@gmail.com>
2019-12-02 13:50:58 -0500

    login edited

--- controller/client.js
+++ controller/client.js
@@ -2,13 +2,25 @@ const jwt = require('jsonwebtoken');
 const client = require('../models/client');
 const User = require('../models/User');
 
-exports.login = function (request, response, next) {
-    client.login(request, response, next).then(function (data) {
-        response.status(200).json(data);
-    })
-    .catch(function (err) {
-        response.status(500).json(err);
-    });
+exports.login = async function (req, res, next) {
+    const post = {
+        email: req.body.email,
+        password: req.body.password
+    };
+
+    try {
+        const loginUser = await User.findOne(post);
+        if (loginUser != null) {
+            const token = jwt.sign({ data: loginUser }, 'secret', { expiresIn: '30m' });
+            res.json({id_token: token});
+        }
+        else {
+            res.json({message: 'User not found'});
+        }
+    }
+    catch (err) {
+        res.json(err);
+    }
 };
 
 exports.register = async function (req, res, next) {
